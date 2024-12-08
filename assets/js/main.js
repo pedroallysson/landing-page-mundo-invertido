4b3ccc0f6784e484ddf5376ad937a6fbcbda197d
Pedro <pedro.alysson@hotmail.com>
2024-12-08 01:05:15 -0300

    refactor: improved button action code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,8 @@
 
 
-let tema = document.querySelector("body");
-
 function switchTheme(){
-    if (tema.classList.contains("light-theme")){
-        tema.classList.remove("light-theme");
-        tema.classList.add("dark-theme");
-    }else {
-        tema.classList.remove("dark-theme");
-        tema.classList.add("light-theme");
-    }
+    document.body.classList.toggle("dark-theme");
+    document.body.classList.toggle("light-theme");
 }
 
 let botao = document.getElementById("switch-theme-button");
